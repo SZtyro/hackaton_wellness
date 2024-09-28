15929ca1b8883afd4f64a5cf0cdeed382e872b0d
MckCieply <mwppl123@gmail.com>
2024-09-29 01:41:03 +0200

    minor visual fixes

--- src/main/frontend/src/app/component/achievements/achievements.component.ts
+++ src/main/frontend/src/app/component/achievements/achievements.component.ts
@@ -10,7 +10,9 @@ export class AchievementsComponent {
   achievements = [
     {icon: 'landscape', progress: 30, title: 'Sudety'},
     {icon: 'elevation', progress: 70, title: 'Strome podejście'},
-    {icon: 'avg_time', progress: 22, title: 'Długi trening'}
+    {icon: 'avg_time', progress: 22, title: 'Długi trening'},
+    {icon: 'landscape', progress: 80, title: 'Beskid Śląski'},
+    {icon: 'star_half', progress: 50, title: 'Twórca'},
   ];
 
 }
