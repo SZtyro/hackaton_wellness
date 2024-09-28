a4f609acb29b1f7cdaeecf73329d3a916a94b621
MckCieply <mwppl123@gmail.com>
2024-09-28 15:39:18 +0200

    Gradable interface init

---
+++ src/main/java/pl/drunkcom/wellness/interfaces/Gradable.java
@@ -0,0 +1,13 @@
+package pl.drunkcom.wellness.interfaces;
+
+public interface Gradable {
+
+    public int getCountOfGrades();
+
+    public double getAverageGrade();
+
+    public void setCountOfGrades(int countOfGrades);
+
+    public void setAverageGrade(double averageGrade);
+
+}
