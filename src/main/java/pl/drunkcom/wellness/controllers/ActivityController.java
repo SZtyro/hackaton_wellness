52cb8cc00fe7cdbe6cdf8e00a13b7f373422686b
MckCieply <mwppl123@gmail.com>
2024-09-29 06:47:53 +0200

    Override getAll to pull only w/o parent

--- src/main/java/pl/drunkcom/wellness/controllers/ActivityController.java
+++ src/main/java/pl/drunkcom/wellness/controllers/ActivityController.java
@@ -1,6 +1,7 @@
 package pl.drunkcom.wellness.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -17,9 +18,11 @@ import pl.drunkcom.wellness.services.AppUserService;
 import pl.drunkcom.wellness.services.TaskService;
 
 import javax.websocket.server.PathParam;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -36,7 +39,16 @@ public class ActivityController extends BaseController<Activity, ActivityReposit
 
     @Autowired
     private TaskService taskService;
-
+    @GetMapping()
+    @SuppressWarnings("unchecked")
+    public ResponseEntity<List<Activity>> getAll(){
+        List<Activity> entities = new ArrayList<>();
+        for(Activity a : this.service.getAll()){
+            if(a.getParent() == null)
+                entities.add(a);
+        }
+        return new ResponseEntity<>(entities, HttpStatus.OK);
+    }
 
     @PostMapping("/addGrade")
     public ResponseEntity<Activity> addGrade(Long id, Double grade){
