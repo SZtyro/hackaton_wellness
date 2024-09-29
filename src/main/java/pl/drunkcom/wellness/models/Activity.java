dafd5791013202407835e55c2a72c0edf7c09165
MckCieply <mwppl123@gmail.com>
2024-09-29 03:55:18 +0200

    Moved author to generic

--- src/main/java/pl/drunkcom/wellness/models/Activity.java
+++ src/main/java/pl/drunkcom/wellness/models/Activity.java
@@ -16,9 +16,6 @@ public class Activity extends BaseEntity implements Gradable {
     @Column
     private String name;
 
-    @Column
-    private String author;
-
     @OneToMany
     private List<Task> tasks;
 
